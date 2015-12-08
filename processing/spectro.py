c955feaf16b4cc728a8a9af3d2d655802b63c10c
apedley <apedley@gmail.com>
2015-12-08 16:09:19 -0600

    Added final processing script

--- processing/spectro.py
+++ processing/spectro.py
@@ -1,36 +1,25 @@
 #!/usr/bin/env python
 
+import sys
 from scipy.io.wavfile import read,write
-from pylab import plot,show,subplot,specgram
+from pylab import plot,show,subplot,specgram,savefig
 
 
+def usage():
+    print "USAGE: spectro.py <input wav path> <output image path>"
+    sys.exit(0)
 
-rate,data = read('umbrellaonefiltered.wav')
-subplot(411)
-# plot(range(len(data)),data)
-# subplot(412)
-# NFFT is the number of data points used in each block for the FFT
-# and noverlap is the number of points of overlap between blocks
-# specgram(data, NFFT=128, noverlap=0) # small window
-# subplot(413)
-# specgram(data, NFFT=512, noverlap=0) 
-# subplot(414)
-specgram(data, NFFT=1024, noverlap=0) # big window
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        usage()
+    output_filepath = sys.argv[2]
+    input_filepaths = sys.argv[1]
 
-rate,data = read('umbrellatwofiltered.wav')
-subplot(412)
-specgram(data, NFFT=1024, noverlap=0)
-
-rate,data = read('umbrellathreefiltered.wav')
-subplot(413)
-specgram(data, NFFT=1024, noverlap=0)
-
-
-rate,data = read('omg.wav')
-subplot(410)
+rate,data = read(input_filepaths)
+# subplot(411)
 specgram(data, NFFT=1024, noverlap=0)
 
 # rate,data = read('umbrellafourfiltered.wav')
 # subplot(414)
 # specgram(data, NFFT=1024, noverlap=0)
-show()
+savefig(output_filepath)
