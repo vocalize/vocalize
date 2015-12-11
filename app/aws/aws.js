0e0c34dcf8e57e5585a754e827962a2e22dde866
Alex Zywiak <alex.zywiak@gmail.com>
2015-12-11 13:45:14 -0500

    Added functionality to get and upload words on S3 and the DB

--- app/aws/aws.js
+++ app/aws/aws.js
@@ -2,7 +2,14 @@ var s3 = require('s3');
 var BbPromise = require('bluebird');
 var path = require('path');
 var fs = require('fs');
+var mongoose = require('mongoose');
 var config = require('./config');
+var util = require('../util');
+
+var readFile = BbPromise.promisify(fs.readFile);
+var readdir = BbPromise.promisify(fs.readdir);
+
+var Word = require('../models/word');
 
 var client = s3.createClient({
   s3Options: {
@@ -11,16 +18,82 @@ var client = s3.createClient({
   }
 });
 
+exports.addWordsByDir = function(directory){
+
+  var params,
+      metadataFile = path.join(directory, 'meta.json');
+      
+  return readFile(metadataFile)
+    .then(function(data){
+
+      params = JSON.parse(data);
+      return readdir(directory);
+    })
+    .then(function(files){
+      
+      files = files.filter(function(file){
+        return path.parse(file).ext === '.wav';
+      });
+
+      return BbPromise.each(files, function(file){
+        var filepath = path.join(directory, file);
+        return exports.addWord(filepath, params);
+      });
+
+    })
+    .then(console.log.bind(this, 'Words successfully added from ' + directory))
+    .catch(function(err){
+      console.log('Error adding files from ' + directory);
+    });
+};
+
+/**
+ * Add a new word to the app
+ * Uploads file to Amazon S3 and saves the word to the DB
+ * Filename must be unique!
+ * @param  {[string]} file   [file path of wav file]
+ * @param  {[object]} params [{word:String, language: String, accent: String, gender: String}]
+ * @return {[promise]}       [resolves on successful save]
+ */
+exports.addWord = function(file, params) {
+
+  mongoose.connect('mongodb://localhost/vocalize');
+
+  mongoose.connection.on('connected', function() {
+    console.log('Connected to DB');
+
+    var word = new Word({
+      word: params.word,
+      language: params.language,
+      accent: params.accent,
+      gender: params.gender,
+      s3: {
+        Bucket: config.bucket,
+        Key: path.parse(file).base
+      }
+    });
+
+    return exports.uploadFile(file)
+      .then(word.save)
+      .then(function(){
+        console.log(file + ' successfully saved and uploaded!');
+      }).finally(function(){
+        mongoose.connection.close();
+      }).catch(function(err){
+        console.error(err);
+      });
+  });
+};
+
 /**
  * Upload a file to Amazon S3
  * File assumed to be in the same directory
  * @param  {[string]} filename [file to be upload]
  * @return {[promise]}         [resolves on successful upload]
  */
-exports.uploadFile = function(filename) {
+exports.uploadFile = function(filepath) {
   return new BbPromise(function(resolve, reject) {
-    var filepath = path.join(__dirname, filename);
-
+    console.log('uploading ' + filepath);
     var params = {
       localFile: filepath,
 
@@ -52,7 +125,7 @@ exports.uploadFile = function(filename) {
  * @return {[stream]}        [pipes stream to the response]
  */
 exports.downloadStream = function(req, res, next) {
-  
+
   var params = {
     Bucket: config.bucket,
     Key: req.params.filename
--- app/routes.js
+++ app/routes.js
@@ -4,14 +4,28 @@ var router = express.Router();
 
 var aws = require('./aws/aws');
 var helpers = require('./request-handler');
+var wordController = require('./controllers/words');
 
 // MIDDLEWARE
 router.use(bodyParser.json()); // automatically parses the body of all POST requests
 router.use(bodyParser.urlencoded({extended: true})); // only accept url encoded requests
 
 // ROUTES
-router.get('/api/next', helpers.retrieveNextWord);
+/**
+ * Get Single Word by Query String
+ */
+router.get('/api/word/', wordController.getWord);
 
+/**
+ * Get next word by index
+ * Must satisfy other query parameters: language etc.
+ */
+router.get('/api/word/index/', wordController.getWordByNextIndex);
+
+/**
+ * Stream file from S3 to response
+ * Include S3 filename as a parameter
+ */
 router.get('/api/audio/:filename', aws.downloadStream);
 
 router.post('/api/audio', function(req,res) {
