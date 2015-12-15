8cde7dd27d15c24ce8f4ac2f1844e63eeb798ac2
apedley <apedley@gmail.com>
2015-12-15 11:10:18 -0600

    Started python tests

---
+++ processing/processing.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+import numpy as np
+import subprocess as sp
+from scipy.io.wavfile import read as wavread
+from scipy.io.wavfile import write as wavwrite
+from scipy.signal.filter_design import butter, buttord
+from scipy.signal import lfilter, lfiltic
+from pydub import AudioSegment
+
+
+def average():
+
+def match_length():
+
+def trim():
+
+def chop():
+
+def filter():
+
+def create_spectrogram():
+
+def get_score():
