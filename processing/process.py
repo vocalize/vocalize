c1970ac102461330655cce408bee1f95cc595997
apedley <apedley@gmail.com>
2015-12-21 14:06:49 -0600

    Processing fixes

--- processing/process.py
+++ processing/process.py
@@ -3,13 +3,11 @@
 
 import sys
 import imp
-processing = imp.load_source('processing', '../../speechprocessing/speechprocessing/processing.py')
-
-
+import speechprocessing
 
 
 def usage():
-  print "USAGE: process.py <input file path> <output file path> <control file path>"
+  print "USAGE: process.py <input file path> <control file path>"
   sys.exit(0)
 
 if __name__ == '__main__':
@@ -17,14 +15,14 @@ if __name__ == '__main__':
   if len(sys.argv) < 2:
     usage()
   input_wav_path = sys.argv[1]
-  output_wav_path = sys.argv[2]
-  control_wav_path = sys.argv[3]
+  control_wav_path = sys.argv[2]
 
-  processing.chop(input_wav_path, output_wav_path)
-  processing.trim(output_wav_path, output_wav_path)
-  processing.match_length(output_wav_path, output_wav_path, control_wav_path)
-  processing.filter(output_wav_path, output_wav_path)
-  distance = processing.compare(control_wav_path, output_wav_path)
+  speechprocessing.chop(input_wav_path, input_wav_path)
+  speechprocessing.trim(input_wav_path, input_wav_path)
+  speechprocessing.match_length(input_wav_path, input_wav_path, control_wav_path, force=True)
+  speechprocessing.filter(input_wav_path, input_wav_path)
+  distance = speechprocessing.compare(control_wav_path, input_wav_path)
+
   print distance
 
 
