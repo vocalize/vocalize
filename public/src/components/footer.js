9f88fd5380be001e598286d7ea3d017f4264a9c0
Alex Zywiak <alex.zywiak@gmail.com>
2015-12-29 20:23:05 -0500

    Finished up UI styles

---
+++ public/src/components/footer.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Footer = React.createClass({
+
+	render: function(){
+		return(
+			<footer>
+				<h3>Vocalize</h3>
+			</footer>
+		)
+	}
+});
+
+module.exports = Footer;
--- public/src/components/targetWord.js
+++ public/src/components/targetWord.js
@@ -1,12 +1,18 @@
 var React = require('react');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group'); 
 
 var TargetWord = React.createClass({
   render: function(){
+
+    var target = <h1 key={this.props.targetWord} className="target-word center-content"> {this.props.targetWord} </h1>;
+    
     return (
       <div>
-        <h1 className="target-word center-content">
-          {this.props.targetWord}
-        </h1>
+          <div className="small-bucket">
+            <ReactCSSTransitionGroup transitionName="fade" transitionEnterTimeout={500} transitionLeaveTimeout={1}>
+              {target}
+            </ReactCSSTransitionGroup>
+          </div>
         <hr className="separator" />
       </div>
     );
